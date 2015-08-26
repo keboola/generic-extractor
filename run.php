91a03b79f2b2526c2d7b0c98275a585a38921389
Ondrej Vana <ondrej.vana@keboola.com>
2015-08-25 19:39:20 -0700

    fix: catch PHP errors and notices

--- run.php
+++ run.php
@@ -7,7 +7,7 @@ use	Keboola\Juicer\Common\Logger,
 	Keboola\Juicer\Exception\ApplicationException,
 	Keboola\Juicer\Exception\UserException;
 
-require_once(dirname(__FILE__) . "/vendor/autoload.php");
+require_once(dirname(__FILE__) . "/bootstrap.php");
 
 const APP_NAME = 'ex-generic-v2';
 
@@ -50,8 +50,19 @@ try {
 	exit(1);
 } catch(ApplicationException $e) {
 	Logger::log('error', $e->getMessage(), (array) $e->getData());
-	exit($e->getCode() ?: 2);
+	exit($e->getCode() > 1 ? $e->getCode(): 2);
+} catch(ErrorException $e) {
+	Logger::log('error', $e->getMessage(), [
+		'errFile' => $e->getFile(),
+		'errLine' => $e->getLine(),
+		'trace' => $e->getTrace()
+	]);
+	exit(2);
+} catch(Exception $e) {
+	Logger::log('error', $e->getMessage());
+	exit(2);
 }
+
 
 Logger::log('info', "Extractor finished successfully.");
 exit(0);
