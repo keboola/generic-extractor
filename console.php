a6b7b8847a3e259ae9438dbaf205667fab9a626e
Ondrej Vana <ondrej.vana@keboola.com>
2015-11-11 11:11:32 -0800

    fix: console.php includes bootstrap noq

--- console.php
+++ console.php
@@ -1,7 +1,9 @@
 #!/usr/bin/env php
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+// require __DIR__.'/vendor/autoload.php';
+require_once(dirname(__FILE__) . "/bootstrap.php");
+
 
 use Keboola\GenericExtractor\Command\ModuleCommand;
 use Symfony\Component\Console\Application;
