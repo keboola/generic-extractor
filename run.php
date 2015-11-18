e19c767ea53c29be596329966cda079379580078
Ondrej Vana <ondrej.vana@keboola.com>
2015-11-18 10:41:33 -0800

    fix: UserExceptions from within guzzle subscriber are caught

--- run.php
+++ run.php
@@ -9,21 +9,32 @@ require_once(dirname(__FILE__) . "/bootstrap.php");
 
 const APP_NAME = 'ex-generic-v2';
 
+function userError(UserException $e) {
+    Logger::log('error', $e->getMessage(), (array) $e->getData());
+    exit(1);
+}
+
+
 // TODO create an Exception handler, register in bootstrap and handle the try/catch there?
 try {
     $executor = new Executor;
     $executor->run();
 } catch(UserException $e) {
-    Logger::log('error', $e->getMessage(), (array) $e->getData());
-    exit(1);
+    userError($e);
 } catch(ApplicationException $e) {
     Logger::log('error', $e->getMessage(), (array) $e->getData());
     exit($e->getCode() > 1 ? $e->getCode(): 2);
 } catch(Exception $e) {
+    if ($e instanceof \GuzzleHttp\Exception\RequestException
+        && $e->getPrevious() instanceof UserException) {
+        userError($e->getPrevious());
+    }
+
     Logger::log('error', $e->getMessage(), [
         'errFile' => $e->getFile(),
         'errLine' => $e->getLine(),
-        'trace' => $e->getTrace()
+        'trace' => $e->getTrace(),
+        'exception' => get_class($e)
     ]);
     exit(2);
 }
