3c6a044f32f897659150f7013f066e0e0ecd861b
Odin <odinuv@gmail.com>
2016-12-28 10:46:48 +0100

    chore: build on ECR

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/generic-extractor quay.io/keboola/generic-extractor:$TRAVIS_TAG
-docker tag keboola/generic-extractor quay.io/keboola/generic-extractor:latest
-docker images
-docker push quay.io/keboola/generic-extractor:$TRAVIS_TAG
-docker push quay.io/keboola/generic-extractor:latest
+# install aws cli w/o sudo
+pip install --user awscli
+# put aws in the path
+export PATH=$PATH:$HOME/.local/bin
+# needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars 
+eval $(aws ecr get-login --region us-east-1)
+docker tag keboola/docker-demo-app:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:$TRAVIS_TAG
+docker tag keboola/docker-demo-app:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:latest
+docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:$TRAVIS_TAG
+docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:latest
