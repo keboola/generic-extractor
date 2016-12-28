5d9242848777b05d586b8d9aa8702c1226ac780f
Odin <odinuv@gmail.com>
2016-12-28 23:31:07 +0100

    fix: tag names

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 # needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars 
 eval $(aws ecr get-login --region us-east-1)
-docker tag keboola/docker-demo-app:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:$TRAVIS_TAG
-docker tag keboola/docker-demo-app:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:latest
+docker tag keboola/generic-extractor:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:$TRAVIS_TAG
+docker tag keboola/generic-extractor:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:latest
 docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:$TRAVIS_TAG
 docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/ex-generic-v2:latest
