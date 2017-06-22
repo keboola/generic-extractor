257d629662aa78287548f21811ac16ece584e673
Odin <odinuv@gmail.com>
2017-06-22 11:17:24 +0200

    refactor: merge configuration class from juicer as this was the only child of that class

--- src/Keboola/GenericExtractor/Config/Configuration.php
+++ src/Keboola/GenericExtractor/Config/Configuration.php
@@ -2,18 +2,201 @@
 
 namespace Keboola\GenericExtractor\Config;
 
-use Keboola\Juicer\Config\Configuration as BaseConfiguration;
-use Keboola\Juicer\Exception\FileNotFoundException;
-use Keboola\Juicer\Exception\ApplicationException;
+use Keboola\Juicer\Config\Config;
+use Keboola\Juicer\Config\JobConfig;
 use Keboola\Juicer\Exception\NoDataException;
-use Keboola\Juicer\Filesystem\JsonFile;
+use Keboola\Juicer\Exception\UserException;
+use Keboola\Temp\Temp;
+use Psr\Log\LoggerInterface;
 
 /**
  * {@inheritdoc}
  */
-class Configuration extends BaseConfiguration
+class Configuration
 {
+    /**
+     * @var Temp
+     */
+    protected $temp;
+
+    /**
+     * @var string
+     */
+    protected $dataDir;
+
+    /**
+     * @var []
+     */
+    protected $jsonFiles;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
     const CACHE_TTL = 604800;
+
+    /**
+     * Configuration constructor.
+     * @param $dataDir
+     * @param Temp $temp
+     * @param LoggerInterface $logger
+     */
+    public function __construct($dataDir, Temp $temp, LoggerInterface $logger)
+    {
+        $this->temp = $temp;
+        $this->dataDir = $dataDir;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @return Config[]
+     */
+    public function getMultipleConfigs()
+    {
+        try {
+            $iterations = $this->getJSON('/config.json', 'parameters', 'iterations');
+        } catch (NoDataException $e) {
+            $iterations = [null];
+        }
+
+        $configs = [];
+        foreach ($iterations as $params) {
+            $configs[] = $this->getConfig($params);
+        }
+
+        return $configs;
+    }
+
+    /**
+     * @param array $params Values to override in the config
+     * @return Config
+     * @throws UserException
+     */
+    public function getConfig(array $params = null)
+    {
+        try {
+            $configJson = $this->getJSON('/config.json', 'parameters', 'config');
+        } catch (NoDataException $e) {
+            throw new UserException($e->getMessage(), 0, $e);
+        }
+
+        if (!is_null($params)) {
+            $configJson = array_replace($configJson, $params);
+        }
+
+        $configName = empty($configJson['id']) ? '' : $configJson['id'];
+        $runtimeParams = []; // TODO get runtime params from console
+
+        if (empty($configJson['jobs'])) {
+            throw new UserException("No 'jobs' specified in the config!");
+        }
+
+        $jobs = $configJson['jobs'];
+        $jobConfigs = [];
+        foreach ($jobs as $job) {
+            $jobConfig = $this->createJob($job);
+            $jobConfigs[$jobConfig->getJobId()] = $jobConfig;
+        }
+        unset($configJson['jobs']); // weird
+
+        $config = new Config($configName, $runtimeParams);
+        $config->setJobs($jobConfigs);
+        $config->setAttributes($configJson);
+
+        return $config;
+    }
+
+    /**
+     * @param object $job
+     * @return JobConfig
+     * @throws UserException
+     */
+    protected function createJob($job)
+    {
+        if (!is_array($job)) {
+            throw new UserException("Invalid format for job configuration.", 0, null, ['job' => $job]);
+        }
+
+        return JobConfig::create($job);
+    }
+
+    /**
+     * @return array
+     */
+    public function getMetadata()
+    {
+        return json_decode(file_get_contents($this->dataDir . "/in/state.json"), true);
+    }
+
+    public function saveConfigMetadata(array $data)
+    {
+        $dirPath = $this->dataDir . '/out';
+
+        if (!is_dir($dirPath)) {
+            mkdir($dirPath, 0700, true);
+        }
+
+        file_put_contents($dirPath . '/state.json', json_encode($data));
+    }
+
+
+    /**
+     * @param string $filePath
+     * @return mixed
+     */
+    protected function getJSON($filePath)
+    {
+        $path = func_get_args();
+        $filePath = array_shift($path);
+
+        if (empty($this->jsonFiles[$filePath])) {
+            $this->jsonFiles[$filePath] = json_decode(file_get_contents($this->dataDir . $filePath), true);
+        }
+
+        return call_user_func_array([$this->jsonFiles[$filePath], 'get'], $path);
+    }
+
+    /**
+     * @param Table[] $csvFiles
+     * @param string $bucketName
+     * @param bool $sapiPrefix whether to prefix the output bucket with "in.c-"
+     * @param bool $incremental Set the incremental flag in manifest
+     */
+    public function storeResults(array $csvFiles, $bucketName = null, $sapiPrefix = true, $incremental = false)
+    {
+        $path = "{$this->dataDir}/out/tables/";
+
+        if (!is_null($bucketName)) {
+            $path .= $bucketName . '/';
+            $bucketName = $sapiPrefix ? 'in.c-' . $bucketName : $bucketName;
+        }
+
+        if (!is_dir($path)) {
+            mkdir($path, 0775, true);
+            chown($path, fileowner("{$this->dataDir}/out/tables/"));
+            chgrp($path, filegroup("{$this->dataDir}/out/tables/"));
+        }
+
+        foreach ($csvFiles as $key => $file) {
+            $manifest = [];
+
+            if (!is_null($bucketName)) {
+                $manifest['destination'] = "{$bucketName}.{$key}";
+            }
+
+            $manifest['incremental'] = is_null($file->getIncremental())
+                ? $incremental
+                : $file->getIncremental();
+
+            if (!empty($file->getPrimaryKey())) {
+                $manifest['primary_key'] = $file->getPrimaryKey(true);
+            }
+
+            file_put_contents($path . $key . '.manifest', json_encode($manifest));
+            copy($file->getPathname(), $path . $key);
+        }
+    }
 
     /**
      * @return array
@@ -56,70 +239,5 @@ class Configuration extends BaseConfiguration
         } catch (NoDataException $e) {
             return [];
         }
-    }
-
-    /**
-     * @return array
-     * @throws ApplicationException
-     * @todo 'tis flawed - the path shouldn't be hardcoded for tests
-     */
-    public function getModules()
-    {
-        $modules = ['response' => []];
-
-        try {
-            $modulesCfg = JsonFile::create(__DIR__ . '/../../../../config/modules.json')->getData();
-        } catch (FileNotFoundException $e) {
-            $modulesCfg = [];
-        }
-
-        foreach ($modulesCfg as $moduleCfg) {
-            $module = $this->createModule($moduleCfg);
-            if (isset($modules[$module['type']][$module['level']])) {
-                throw new ApplicationException(
-                    "Multiple modules cannot share the same 'level'",
-                    0,
-                    null,
-                    [
-                        'newModule' => $moduleCfg['class'],
-                        'existingModule' => gettype($modules[$module['type']][$module['level']])
-                    ]
-                );
-            }
-
-            $modules[$module['type']][$module['level']] = $module['class'];
-        }
-
-        foreach ($modules as $type => &$typeModules) {
-            ksort($typeModules);
-        }
-
-        return $modules;
-    }
-
-    /**
-     * @param $config
-     * @return array
-     * @throws ApplicationException
-     */
-    protected function createModule($config)
-    {
-        if (empty($config['type'])) {
-            throw new ApplicationException("Module 'type' not set!");
-        }
-
-        if (!isset($config['level'])) {
-            $config['level'] = 9999999;
-        }
-
-        if (!class_exists($config['class'])) {
-            throw new ApplicationException("Class '{$config['class']}' not found!");
-        }
-
-        return [
-            'class' => new $config['class']($this->logger, isset($config['config']) ? $config['config'] : null),
-            'type' => $config['type'],
-            'level' => $config['level']
-        ];
     }
 }
--- tests/Config/ConfigurationTest.php
+++ tests/Config/ConfigurationTest.php
@@ -2,40 +2,175 @@
 
 namespace Keboola\GenericExtractor\Tests\Config;
 
-use Keboola\GenericExtractor\Config\Configuration;
-use Keboola\GenericExtractor\Response\FindResponseArray;
-use Keboola\GenericExtractor\Tests\ExtractorTestCase;
-use Keboola\Juicer\Filesystem\JsonFile;
+use Keboola\Juicer\Config\Config;
+use Keboola\Juicer\Config\Configuration;
+use Keboola\Juicer\Config\JobConfig;
+use Keboola\Juicer\Exception\UserException;
+use Keboola\Juicer\Tests\ExtractorTestCase;
 use Keboola\Temp\Temp;
+use Keboola\CsvTable\Table;
 use Psr\Log\NullLogger;
+use Symfony\Component\Filesystem\Filesystem;
 
 class ConfigurationTest extends ExtractorTestCase
 {
-    public function testCreateModule()
+    public function testStoreResults()
     {
-        $config = [
-            'class' => FindResponseArray::class,
-            'type' => 'response',
-            'level' => 100
-        ];
+        $resultsPath = './data/storeResultsTest' . uniqid();
 
-        $configuration = new Configuration('./Tests/data/getPost', new Temp(), new NullLogger());
-
-        $module = self::callMethod($configuration, 'createModule', [$config]);
-
-        self::assertInstanceOf($config['class'], $module['class']);
+        $this->storeResults($resultsPath, 'full', false);
     }
 
-    public function testGetModules()
+    public function testIncrementalResults()
     {
-        $configuration = new Configuration('./Tests/data/getPost', new Temp(), new NullLogger());
+        $resultsPath = './data/storeResultsTest' . uniqid();
 
-        $modules = $configuration->getModules();
+        $this->storeResults($resultsPath, 'incremental', true);
+    }
 
-        $modulesCfg = JsonFile::create(__DIR__ . '/../../config/modules.json')->getData();
+    public function testDefaultBucketResults()
+    {
+        $resultsPath = './data/storeResultsDefaultBucket' . uniqid();
 
-        foreach ($modulesCfg as $moduleCfg) {
-            self::assertInstanceOf($moduleCfg['class'], $modules[$moduleCfg['type']][$moduleCfg['level']]);
+        $configuration = new Configuration($resultsPath, new Temp(), new NullLogger());
+
+        $files = [
+            Table::create('first', ['col1', 'col2']),
+            Table::create('second', ['col11', 'col12'])
+        ];
+
+        $files[0]->writeRow(['a', 'b']);
+        $files[1]->writeRow(['c', 'd']);
+        $files[1]->setPrimaryKey(['col11']);
+
+        $configuration->storeResults($files);
+
+        foreach (new \DirectoryIterator(__DIR__ . '/../data/storeResultsDefaultBucket/out/tables/') as $file) {
+            self::assertFileEquals($file->getPathname(), $resultsPath . '/out/tables/' . $file->getFilename());
         }
+
+        $this->rmDir($resultsPath);
+    }
+
+    protected function storeResults($resultsPath, $name, $incremental)
+    {
+        $configuration = new Configuration($resultsPath, new Temp('test'), new NullLogger());
+
+        $files = [
+            Table::create('first', ['col1', 'col2']),
+            Table::create('second', ['col11', 'col12'])
+        ];
+
+        $files[0]->writeRow(['a', 'b']);
+        $files[1]->writeRow(['c', 'd']);
+
+        $configuration->storeResults($files, $name, true, $incremental);
+
+        foreach (new \DirectoryIterator(__DIR__ . '/../data/storeResultsTest/out/tables/' . $name) as $file) {
+            self::assertFileEquals($file->getPathname(), $resultsPath . '/out/tables/' . $name . '/' . $file->getFilename());
+        }
+
+        $this->rmDir($resultsPath);
+    }
+
+    public function testGetConfigMetadata()
+    {
+        $path = __DIR__ . '/../data/metadataTest';
+
+        $configuration = new Configuration($path, new Temp('test'), new NullLogger());
+        $json = $configuration->getMetadata();
+
+        self::assertEquals(json_decode('{"some":"data","more": {"woah": "such recursive"}}', true), $json);
+
+        $noConfiguration = new Configuration('asdf', new Temp('test'), new NullLogger());
+        self::assertEquals(null, $noConfiguration->getMetadata());
+    }
+
+    public function testSaveConfigMetadata()
+    {
+        $resultsPath = './data/metadataTest' . uniqid();
+
+        $configuration = new Configuration($resultsPath, new Temp('test'), new NullLogger());
+
+        $configuration->saveConfigMetadata([
+            'some' => 'data',
+            'more' => [
+                'woah' => 'such recursive'
+            ]
+        ]);
+
+        self::assertFileEquals(__DIR__ . '/../data/metadataTest/out/state.json', $resultsPath . '/out/state.json');
+
+        $this->rmDir($resultsPath);
+    }
+
+    public function testGetConfig()
+    {
+        $configuration = new Configuration(__DIR__ . '/../data/recursive', new Temp('test'), new NullLogger());
+
+        $config = $configuration->getConfig();
+
+        $json = json_decode(file_get_contents(__DIR__ . '/../data/recursive/config.json'), true);
+
+        $jobs = $config->getJobs();
+        self::assertEquals(JobConfig::create($json['parameters']['config']['jobs'][0]), reset($jobs));
+
+        self::assertEquals($json['parameters']['config']['outputBucket'], $config->getAttribute('outputBucket'));
+    }
+
+    public function testGetMultipleConfigs()
+    {
+        $configuration = new Configuration(__DIR__ . '/../data/iterations', new Temp('test'), new NullLogger());
+
+        $configs = $configuration->getMultipleConfigs();
+
+        $json = json_decode(file_get_contents(__DIR__ . '/../data/iterations/config.json'), true);
+
+        foreach ($json['parameters']['iterations'] as $i => $params) {
+            self::assertEquals(array_replace(['id' => $json['parameters']['config']['id']], $params), $configs[$i]->getAttributes());
+        }
+        self::assertEquals($configs[0]->getJobs(), $configs[1]->getJobs());
+        self::assertContainsOnlyInstancesOf(Config::class, $configs);
+        self::assertCount(count($json['parameters']['iterations']), $configs);
+    }
+
+    public function testGetMultipleConfigsSingle()
+    {
+        $configuration = new Configuration(__DIR__ . '/../data/simple_basic', new Temp(), new NullLogger());
+        $configs = $configuration->getMultipleConfigs();
+        self::assertContainsOnlyInstancesOf(Config::class, $configs);
+        self::assertCount(1, $configs);
+        self::assertEquals($configuration->getConfig(), $configs[0]);
+    }
+
+    public function testGetJson()
+    {
+        $configuration = new Configuration(__DIR__ . '/../data/simple_basic', new Temp(), new NullLogger());
+
+        $result = self::callMethod($configuration, 'getJson', ['/config.json', 'parameters', 'config', 'id']);
+
+        self::assertEquals('multiCfg', $result);
+    }
+
+    public function testGetInvalidConfig()
+    {
+        $temp = new Temp();
+        $fs = new Filesystem();
+        $fs->dumpFile($temp->getTmpFolder() . '/config.json', 'invalidJSON');
+        $configuration = new Configuration($temp->getTmpFolder(), new Temp(), new NullLogger());
+        try {
+            $configuration->getConfig();
+            self::fail("Invalid JSON must cause exception");
+        } catch (UserException $e) {
+            self::assertContains('Invalid JSON: Syntax error', $e->getMessage());
+        }
+    }
+
+    protected function rmDir($dirPath)
+    {
+        foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dirPath, \FilesystemIterator::SKIP_DOTS), \RecursiveIteratorIterator::CHILD_FIRST) as $path) {
+            $path->isDir() && !$path->isLink() ? rmdir($path->getPathname()) : unlink($path->getPathname());
+        }
+        return rmdir($dirPath);
     }
 }
