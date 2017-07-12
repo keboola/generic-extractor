685e4ae363d15c719375515c7e02e2002b8962a8
Odin <odinuv@gmail.com>
2017-07-12 14:48:18 +0200

    chore: make phpstan required

--- phpunit.sh
+++ phpunit.sh
@@ -4,7 +4,7 @@ php --version
 
 echo "Starting tests" >&1
 ./vendor/bin/phpcs --standard=psr2 --ignore=vendor -n . 
-./vendor/bin/phpstan analyse --level=4 src || true
+./vendor/bin/phpstan analyse --level=4 src
 ./vendor/bin/phpunit --coverage-clover ./build/logs/clover.xml
 ./vendor/bin/test-reporter
 
