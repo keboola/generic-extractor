ea5df4e5f656da03af3d3344a5a5df4fb9d2b658
Výborný Adam <mail@adamvyborny.cz>
2024-05-10 12:12:43 +0200

    Display output when assert fails

--- tests/phpunit/MockExecutionTest.php
+++ tests/phpunit/MockExecutionTest.php
@@ -68,7 +68,7 @@ class MockExecutionTest extends TestCase
         $dataDir = __DIR__ . '/data/syncAction';
         $runPhp = __DIR__ . '/../../src/run.php';
         exec("KBC_DATADIR=$dataDir php $runPhp  2>&1", $output, $retval);
-        self::assertEquals(0, $retval);
+        self::assertEquals(0, $retval, implode("\n", $output));
     }
 
     public function testDynamicUserData(): void
